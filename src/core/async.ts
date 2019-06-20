b93ff0ee3a941424295626263fc85c1958eee5e3
saber2pr <saber2pr@gmail.com>
2019-06-20 11:18:57 +0000

    feat(async.ts): use lazy map

--- src/core/async.ts
+++ src/core/async.ts
@@ -1,9 +1,11 @@
 /*
  * @Author: saber2pr
- * @Date: 2019-06-19 20:28:08
- * @Last Modified by: saber2pr
- * @Last Modified time: 2019-06-19 22:49:40
+ * @Date: 2019-06-20 10:36:45
+ * @Last Modified by:   saber2pr
+ * @Last Modified time: 2019-06-20 10:36:45
  */
+import { from } from './lazy'
+
 export const async_reduce = async <T, R>(
   items: T[],
   reducer: (receiver: R, value: T) => Promise<R>,
@@ -17,10 +19,7 @@ export const async_reduceRight = async <T, R>(
   items: T[],
   reducer: (receiver: R, value: T) => Promise<R>,
   initValue: R
-) => {
-  for (const item of items.reverse()) initValue = await reducer(initValue, item)
-  return initValue
-}
+) => async_reduce(items.reverse(), reducer, initValue)
 
 export const async_compose = <T>(...fns: ((value: T) => Promise<T>)[]) => (
   value: T
@@ -42,29 +41,30 @@ export const async_setter = async <T, K extends keyof T>(
 
 export const async_forEach = <T, R = void>(
   items: T[],
-  callbackFn: (value: T, index: number, array: T[]) => Promise<R>
-) => Promise.all(items.map(callbackFn))
+  callbackFn: (value: T) => Promise<R>
+) =>
+  Promise.all(
+    from(items)
+      .map(callbackFn)
+      .to()
+  )
 
 export const async_filter = async <T>(
   items: T[],
-  callbackFn: (value: T, index: number, array: T[]) => Promise<boolean>
+  callbackFn: (value: T) => Promise<boolean>
 ) => {
   const next: T[] = []
-  await async_forEach(items, (value, index, array) =>
-    callbackFn(value, index, array).then(_ => _ && next.push(value))
+  await async_forEach(items, value =>
+    callbackFn(value).then(_ => _ && next.push(value))
   )
   return next
 }
 
 export const async_map = async <T, R>(
   items: T[],
-  callbackFn: (value: T, index: number, array: T[]) => Promise<R>
+  callbackFn: (value: T) => Promise<R>
 ) => {
   const next: R[] = []
-
-  await async_forEach(items, async (value, index, array) => {
-    next.push(await callbackFn(value, index, array))
-  })
-
+  await async_forEach(items, async value => next.push(await callbackFn(value)))
   return next
 }
