62e35ca5651e4f99eab3f8fdd50426ffa5159c2e
saber2pr <saber2pr@gmail.com>
2019-06-21 23:34:40 +0000

    fix(compose.test.ts): fixed type

--- src/test/compose.test.ts
+++ src/test/compose.test.ts
@@ -2,12 +2,12 @@ import { compose } from '..'
 
 describe('sync compose', () => {
   it('compose one func', () => {
-    const increase = (v = 0) => v + 1
+    const increase = v => v + 1
     const result = compose(increase)(1)
     expect(result).toEqual(2)
   })
   it('compose two func', () => {
-    const increase = (v = 0) => v + 1
+    const increase = v => v + 1
     const result = compose(
       increase,
       increase
@@ -15,7 +15,7 @@ describe('sync compose', () => {
     expect(result).toEqual(3)
   })
   it('compose more func', () => {
-    const increase = (v = 0) => v + 1
+    const increase = v => v + 1
     const result = compose(
       increase,
       increase,
@@ -36,7 +36,7 @@ describe('sync compose', () => {
       A,
       B,
       C
-    )(0)
+    )()
     expect(sent).toEqual([C, B, A])
   })
 })
