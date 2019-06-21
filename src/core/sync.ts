3f91d9d0d72de25493016660648120b0a2a1050c
saber2pr <saber2pr@gmail.com>
2019-06-21 23:35:17 +0000

    feat(sync): add compose type

--- src/core/sync.ts
+++ src/core/sync.ts
@@ -6,13 +6,143 @@
  */
 import { from } from './lazy'
 
-export const pipe = <T>(...fns: ((value: T) => T)[]) => (value: T) =>
-  from(fns)
-    .reduce((a, b) => b(a), value)
-    .to()[0]
+export function compose<T1>(fn0: () => T1): () => T1
+export function compose<V0, T1>(fn0: (x0: V0) => T1): (x0: V0) => T1
+export function compose<V0, V1, T1>(
+  fn0: (x0: V0, x1: V1) => T1
+): (x0: V0, x1: V1) => T1
+export function compose<V0, V1, V2, T1>(
+  fn0: (x0: V0, x1: V1, x2: V2) => T1
+): (x0: V0, x1: V1, x2: V2) => T1
 
-export const compose = <T>(...fns: ((value: T) => T)[]) => (value: T) =>
-  pipe(...fns.reverse())(value)
+export function compose<T1, T2>(fn1: (x: T1) => T2, fn0: () => T1): () => T2
+export function compose<V0, T1, T2>(
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0) => T1
+): (x0: V0) => T2
+export function compose<V0, V1, T1, T2>(
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1) => T1
+): (x0: V0, x1: V1) => T2
+export function compose<V0, V1, V2, T1, T2>(
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1, x2: V2) => T1
+): (x0: V0, x1: V1, x2: V2) => T2
+
+export function compose<T1, T2, T3>(
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: () => T1
+): () => T3
+export function compose<V0, T1, T2, T3>(
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x: V0) => T1
+): (x: V0) => T3
+export function compose<V0, V1, T1, T2, T3>(
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1) => T1
+): (x0: V0, x1: V1) => T3
+export function compose<V0, V1, V2, T1, T2, T3>(
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1, x2: V2) => T1
+): (x0: V0, x1: V1, x2: V2) => T3
+
+export function compose<T1, T2, T3, T4>(
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: () => T1
+): () => T4
+export function compose<V0, T1, T2, T3, T4>(
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x: V0) => T1
+): (x: V0) => T4
+export function compose<V0, V1, T1, T2, T3, T4>(
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1) => T1
+): (x0: V0, x1: V1) => T4
+export function compose<V0, V1, V2, T1, T2, T3, T4>(
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1, x2: V2) => T1
+): (x0: V0, x1: V1, x2: V2) => T4
+
+export function compose<T1, T2, T3, T4, T5>(
+  fn4: (x: T4) => T5,
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: () => T1
+): () => T5
+export function compose<V0, T1, T2, T3, T4, T5>(
+  fn4: (x: T4) => T5,
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x: V0) => T1
+): (x: V0) => T5
+export function compose<V0, V1, T1, T2, T3, T4, T5>(
+  fn4: (x: T4) => T5,
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1) => T1
+): (x0: V0, x1: V1) => T5
+export function compose<V0, V1, V2, T1, T2, T3, T4, T5>(
+  fn4: (x: T4) => T5,
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1, x2: V2) => T1
+): (x0: V0, x1: V1, x2: V2) => T5
+
+export function compose<T1, T2, T3, T4, T5, T6>(
+  fn5: (x: T5) => T6,
+  fn4: (x: T4) => T5,
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: () => T1
+): () => T6
+export function compose<V0, T1, T2, T3, T4, T5, T6>(
+  fn5: (x: T5) => T6,
+  fn4: (x: T4) => T5,
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x: V0) => T1
+): (x: V0) => T6
+export function compose<V0, V1, T1, T2, T3, T4, T5, T6>(
+  fn5: (x: T5) => T6,
+  fn4: (x: T4) => T5,
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1) => T1
+): (x0: V0, x1: V1) => T6
+export function compose<V0, V1, V2, T1, T2, T3, T4, T5, T6>(
+  fn5: (x: T5) => T6,
+  fn4: (x: T4) => T5,
+  fn3: (x: T3) => T4,
+  fn2: (x: T2) => T3,
+  fn1: (x: T1) => T2,
+  fn0: (x0: V0, x1: V1, x2: V2) => T1
+): (x0: V0, x1: V1, x2: V2) => T6
+
+export function compose(...fns: ((...value: any) => any)[]) {
+  return (value: any) =>
+    from(fns.reverse())
+      .reduce((a, b) => b(a), value)
+      .to()[0]
+}
 
 export const setter = <T, K extends keyof T>(
   target: T,
